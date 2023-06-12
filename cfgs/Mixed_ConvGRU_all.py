5309baee42792e6a5f1ff8611bd51c2f58848407
marcelllbengs <70664374+marcelllbengs@users.noreply.github.com>
2023-06-12 21:16:50 +0200

    Add files via upload

---
+++ cfgs/Mixed_ConvGRU_all.py
@@ -0,0 +1,43 @@
+﻿import os
+import sys
+import h5py
+import re
+import csv
+import numpy as np
+
+
+
+def init(mdlParams):
+    ### Model Selection ###
+    mdlParams['model_type'] = 'MixedCNN_convGRU_CNN' 
+    mdlParams['convRNN_position'] = 'all' # type of MixedConvGRU (front,middle,end,all)
+    mdlParams['normalization'] = 'BatchNorm'
+    mdlParams['numOut'] = 3
+    mdlParams['spatial_dims'] = '3D'  # use 3D network, i.e., 3DCNN 
+
+    mdlParams['many_to_many'] = True # many-to- many RNN
+    mdlParams['multi_output_m2many'] = True # multi-output regression
+    mdlParams['cg_type'] = 'GRU' # type of ConvRNN GRU or LSTm
+    mdlParams['conv_transition'] = False # use a conv. layer instead of an avg. pooling layer for the transition blocks
+
+    ### Network Parameters ###
+    mdlParams['cg_layers'] = [14] # number of feature maps for the first ConvRNN
+    mdlParams['cg_batchnorm'] = False # use batch norm for ConvRNN
+    mdlParams['num_init_features'] = 10 # number of init. features for the first conv. layers
+    mdlParams['Dense_Growth'] = 10 # growth rate 
+    mdlParams['block_config'] = 2 # number of layers in each densenetblock
+    mdlParams['Dense_Compress'] = 1.25 # divide by this number in transition layer
+
+    mdlParams['init_kernel'] = (3,3,3) # kernel size of the init. conv layer
+    mdlParams['init_stride'] = (2,2,2) # stride of the init. conv layer
+    mdlParams['spatial_padding'] = int((mdlParams['init_kernel'][1] - 1) / 2) # same padding
+    
+    ########################### Select Data  ###########################
+    mdlParams['Data_Type'] = '4D'       
+    mdlParams['input_size'] = [32,32,32] # input size along the spatial dimension
+    mdlParams['Temporal_Window'] = 50 # sequence length used during training
+    mdlParams['Temporal_Window_eval'] = 280 # sequence length during evaluation
+
+    return mdlParams
+
+
---
+++ cfgs/nPath4D.py
@@ -0,0 +1,41 @@
+﻿import os
+import sys
+import h5py
+import re
+import csv
+import numpy as np
+
+
+
+def init(mdlParams):
+    ### Model Selection ###
+    mdlParams['model_type'] = 'nPath_NetnD' 
+    mdlParams['normalization'] = 'BatchNorm'
+    mdlParams['numOut'] = 3
+    mdlParams['spatial_dims'] = '4D' # use 4D network
+
+    ### Network Parameters ###
+    mdlParams['num_init_features'] = 14 # number of init. features for the first conv. layers
+    mdlParams['Dense_Growth'] = 10 # growth rate 
+    mdlParams['block_config'] = (3,3,3,3) # number of layers in each densenetblock
+    mdlParams['conv_last'] = False # replace last densenet block with a simple conv. with s=2.
+
+    mdlParams['Dense_Compress'] = 1.25 # divide by this number in transition layer
+    mdlParams['init_kernel'] = (3,3,3,3) # kernel size of the init. conv layer
+    mdlParams['init_stride'] = (1,1,1,1) # stride of the init. conv layer
+    mdlParams['spatial_padding'] = int((mdlParams['init_kernel'][1] - 1) / 2) # same padding
+    mdlParams['factor_features'] = 2 # feature scaling at the fusion point of the multi-path network
+    mdlParams['conv_pooling'] = False # use a conv. layer instead of an avg. pooling layer for the transition blocks
+    
+    mdlParams['t_downsample'] = 1 # in transition layer of densenet (stride)
+    mdlParams['t_downsample_kernel'] = 1 # kernel size for transition layer
+    
+    ########################### Select Data  ###########################
+    mdlParams['Data_Type'] = '4D'
+    mdlParams['input_size'] = [32,32,32] # input size along the spatial dimension
+    mdlParams['Temporal_Window'] = 5 # temporal dimension, i.e., sequence length
+
+    return mdlParams
+
+
+
